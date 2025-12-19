7834a4bd88712f60e848650c48921c911fa00125
vigo-pallavi <pallavi.vigorousit@gmail.com>
2025-12-19 18:23:08 +0530

    updated

--- src/pages/Socket-iframe/SocketIframe15.js
+++ src/pages/Socket-iframe/SocketIframe15.js
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import { GiClick } from "react-icons/gi";
 import { IoVolumeHighOutline, IoVolumeMuteOutline } from "react-icons/io5";
 import useMatchScoreVoice from "../../middleware/useMatchScoreVoice";
+import { MdSportsCricket } from "react-icons/md";
 
 function SocketIframe15() {
   const { marketId, eventId } = useParams();
@@ -169,13 +170,44 @@ function SocketIframe15() {
                   <td>
                     <div style={{ display: "flex", gap: "5px", alignItems: "center" }}>
                       <div className="">
-                        {matchScoreDetails?.score?.player1_array?.img ? (
+                        {/* {matchScoreDetails?.score?.player1_array?.img ? (
                           <img
                             src={matchScoreDetails?.score?.player1_array?.img}
                             alt="team1"
                             style={{ width: "25px", height: "25px", borderRadius: "1000px" }}
                           />
-                        ) : null}
+                        ) : null} */}
+                        {matchScoreDetails?.score?.player1_array?.img ? (
+                          <img
+                            src={matchScoreDetails.score.player1_array.img}
+                            alt="team1"
+                            style={{ width: "25px", height: "25px", borderRadius: "1000px" }}
+                            className=""
+                          />
+                        ) : (
+                          <svg
+                          className="svgIcon"
+                          viewBox="0 0 420 420"
+                          xmlns="http://www.w3.org/2000/svg"
+                          style={{ marginRight: 4 }}
+                        >
+                          <g>
+                            <path
+                              d="M391.227,20c-2.337,0-4.534,0.91-6.186,2.563L317.96,89.674l12.355,12.355l67.097-67.097c1.653-1.652,2.563-3.849,2.563-6.185c0-2.336-0.91-4.532-2.562-6.185C395.761,20.91,393.564,20,391.227,20z"
+                              fill="rgb(232, 85, 18)"
+                            />
+                            <path
+                              d="M323.236,167.203c12.119-12.118,12.122-31.839,0.007-43.961l-26.495-26.495c-5.875-5.875-13.687-9.11-21.997-9.11c-8.305,0-16.11,3.231-21.979,9.102L22.493,327.017c-8.427,8.426,5.999,30.528,22.969,47.497c15.48,15.481,31.611,25.484,41.094,25.486h0.001c2.685,0,4.659-0.776,6.4-2.519L323.236,167.203z"
+                              fill="rgb(241, 156, 125)"
+                            />
+                            <path
+                              d="M419.975,28.747c0-7.679-2.99-14.898-8.42-20.327C406.125,2.99,398.906,0,391.227,0c-7.679,0-14.898,2.99-20.329,8.422L303.13,76.221c-8.33-5.584-18.123-8.584-28.379-8.584c-13.647,0-26.475,5.313-36.122,14.959L8.351,312.874c-5.902,5.902-14.063,19.258-2.41,42.391c7.078,14.049,18.939,26.951,25.379,33.392C43.024,400.36,65.764,419.997,86.553,420h0.005c7.959,0,15.063-2.897,20.542-8.376l230.278-230.278c17.496-17.496,19.619-44.629,6.378-64.472l67.798-67.798C416.985,43.646,419.975,36.427,419.975,28.747z"
+                              fill="rgb(232, 85, 18)"
+                            />
+                          </g>
+                        </svg>
+                          // <MdSportsCricket className="svgIcon text-gray-400" />
+                        )}
                       </div>
                       {matchScoreDetails?.score?.player1_array?.name
                         ? matchScoreDetails?.score?.player1_array?.name
@@ -212,13 +244,39 @@ function SocketIframe15() {
                   <td>
                     <div style={{ display: "flex", gap: "5px", alignItems: "center" }}>
                       <div className="">
-                        {matchScoreDetails?.score?.player2_array?.img ? (
+                        {
+                        matchScoreDetails?.score?.player2_array?.img ? (
                           <img
                             src={matchScoreDetails?.score?.player2_array?.img}
                             alt="team1"
                             style={{ width: "25px", height: "25px", borderRadius: "1000px" }}
                           />
-                        ) : null}
+                        ) : 
+                        (
+                          <svg
+                          className="svgIcon"
+                          viewBox="0 0 420 420"
+                          xmlns="http://www.w3.org/2000/svg"
+                          style={{ marginRight: 4 }}
+                        >
+                          <g>
+                            <path
+                              d="M391.227,20c-2.337,0-4.534,0.91-6.186,2.563L317.96,89.674l12.355,12.355l67.097-67.097c1.653-1.652,2.563-3.849,2.563-6.185c0-2.336-0.91-4.532-2.562-6.185C395.761,20.91,393.564,20,391.227,20z"
+                              fill="rgb(232, 85, 18)"
+                            />
+                            <path
+                              d="M323.236,167.203c12.119-12.118,12.122-31.839,0.007-43.961l-26.495-26.495c-5.875-5.875-13.687-9.11-21.997-9.11c-8.305,0-16.11,3.231-21.979,9.102L22.493,327.017c-8.427,8.426,5.999,30.528,22.969,47.497c15.48,15.481,31.611,25.484,41.094,25.486h0.001c2.685,0,4.659-0.776,6.4-2.519L323.236,167.203z"
+                              fill="rgb(241, 156, 125)"
+                            />
+                            <path
+                              d="M419.975,28.747c0-7.679-2.99-14.898-8.42-20.327C406.125,2.99,398.906,0,391.227,0c-7.679,0-14.898,2.99-20.329,8.422L303.13,76.221c-8.33-5.584-18.123-8.584-28.379-8.584c-13.647,0-26.475,5.313-36.122,14.959L8.351,312.874c-5.902,5.902-14.063,19.258-2.41,42.391c7.078,14.049,18.939,26.951,25.379,33.392C43.024,400.36,65.764,419.997,86.553,420h0.005c7.959,0,15.063-2.897,20.542-8.376l230.278-230.278c17.496-17.496,19.619-44.629,6.378-64.472l67.798-67.798C416.985,43.646,419.975,36.427,419.975,28.747z"
+                              fill="rgb(232, 85, 18)"
+                            />
+                          </g>
+                        </svg>
+                          // <MdSportsCricket className="svgIcon text-gray-400" />
+                        )
+                        }
                       </div>
                       {matchScoreDetails?.score?.player2_array?.name
                         ? matchScoreDetails?.score?.player2_array?.name
@@ -257,11 +315,11 @@ function SocketIframe15() {
                   <table className="sc_15_table">
                     <thead>
                       <tr className="text-white">
-                        <th>Bowler</th>
-                        <th>Over</th>
-                        <th>Run</th>
-                        <th>Wicket</th>
-                        <th>Economy</th>
+                        <th >Bowler</th>
+                        <th className="minWid">O</th>
+                        <th className="minWid">R</th>
+                        <th className="minWid">W</th>
+                        <th className="minWid">Eco</th>
                       </tr>
                     </thead>
                     <tbody>
@@ -275,7 +333,30 @@ function SocketIframe15() {
                                   alt="team1"
                                   style={{ width: "25px", height: "25px", borderRadius: "1000px" }}
                                 />
-                              ) : null}
+                              ) : (
+                                <svg
+                                className="svgIcon"
+                                viewBox="0 0 420 420"
+                                xmlns="http://www.w3.org/2000/svg"
+                                style={{ marginRight: 4 }}
+                              >
+                                <g>
+                                  <path
+                                    d="M391.227,20c-2.337,0-4.534,0.91-6.186,2.563L317.96,89.674l12.355,12.355l67.097-67.097c1.653-1.652,2.563-3.849,2.563-6.185c0-2.336-0.91-4.532-2.562-6.185C395.761,20.91,393.564,20,391.227,20z"
+                                    fill="rgb(232, 85, 18)"
+                                  />
+                                  <path
+                                    d="M323.236,167.203c12.119-12.118,12.122-31.839,0.007-43.961l-26.495-26.495c-5.875-5.875-13.687-9.11-21.997-9.11c-8.305,0-16.11,3.231-21.979,9.102L22.493,327.017c-8.427,8.426,5.999,30.528,22.969,47.497c15.48,15.481,31.611,25.484,41.094,25.486h0.001c2.685,0,4.659-0.776,6.4-2.519L323.236,167.203z"
+                                    fill="rgb(241, 156, 125)"
+                                  />
+                                  <path
+                                    d="M419.975,28.747c0-7.679-2.99-14.898-8.42-20.327C406.125,2.99,398.906,0,391.227,0c-7.679,0-14.898,2.99-20.329,8.422L303.13,76.221c-8.33-5.584-18.123-8.584-28.379-8.584c-13.647,0-26.475,5.313-36.122,14.959L8.351,312.874c-5.902,5.902-14.063,19.258-2.41,42.391c7.078,14.049,18.939,26.951,25.379,33.392C43.024,400.36,65.764,419.997,86.553,420h0.005c7.959,0,15.063-2.897,20.542-8.376l230.278-230.278c17.496-17.496,19.619-44.629,6.378-64.472l67.798-67.798C416.985,43.646,419.975,36.427,419.975,28.747z"
+                                    fill="rgb(232, 85, 18)"
+                                  />
+                                </g>
+                              </svg>
+                                // <MdSportsCricket className="svgIcon text-gray-400" />
+                              )}
                             </div>
                             {matchScoreDetails?.score?.bowlerArray?.name
                               ? matchScoreDetails?.score?.bowlerArray?.name
